5c9cb7e1c43bf80b727859191ac852a4c73fc103
Ricky McMurtrey <ricky.mcmurtrey@activeprospect.com>
2022-12-05 21:32:39 -0600

    If I keep this up, the elves will make me their king.

---
+++ day5/part2.js
@@ -0,0 +1,29 @@
+import { input } from './input.js';
+
+const stacks = {};
+const moves = [];
+const inputParts = input.split(/\n\n/);
+
+inputParts[0].split(/\n/).forEach((line) => {
+  for(let i = 0; i < line.length; i += 4) {
+    const index = i / 4 + 1;
+    const col = line.substring(i, i + 3).replace(/[\[\]]/g, '');
+    stacks[index] = stacks[index] || [];
+    if (col.match(/[A-Z]/)) stacks[index].unshift(col);
+  }
+});
+
+inputParts[1].split(/\n/).forEach((line) => {
+    const parts = line.split(' ');
+    const [amount, from, to] = [parts[1], parts[3], parts[5]].map(p => parseInt(p));
+    const transfer = [];
+    for(let i = 0; i < amount; i++) {
+      const box = stacks[from].pop();
+      transfer.unshift(box);
+    }
+    stacks[to] = stacks[to].concat(transfer);
+});
+
+const keys = Object.keys(stacks).map(k => parseInt(k));
+const top = keys.reduce((prev, current) => prev + stacks[current].pop(), '');
+console.log(top);
